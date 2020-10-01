e4210e7024d3df9342306b9795ea65e6fac518d2
color <color@gmail.com>
2020-10-01 21:55:53 +0800

    修复问题

--- Sources/fmdatabase/FMResultSet.swift
+++ Sources/fmdatabase/FMResultSet.swift
@@ -1,3 +1,4 @@
+import Foundation
 import CSqlite3
 
 public class FMResultSet {
@@ -11,9 +12,44 @@ public class FMResultSet {
         }
     }
 
+    var resultDict: [String: SqliteValue] {
+        let num_cols = sqlite3_data_count(statement.statement)
+        return [:]
+    }
+
     public  init(db: FMDatabase, statement: FMStatement) {
         self.parentDB = db
         self.statement = statement
+    }
+
+    public func valueForColumnIndex(_ columnIdx: Int32) -> SqliteValue? {
+        let columnType = sqlite3_column_type(statement.statement, columnIdx)
+        if (columnType == SQLITE_INTEGER) {
+            return sqlite3_column_int64(statement.statement, columnIdx);
+        } else if (columnType == SQLITE_FLOAT) {
+            return sqlite3_column_double(statement.statement, columnIdx);
+        } else if (columnType == SQLITE_BLOB) {
+            return dataForColumnIndex(columnIdx)
+        } else  {
+            return nil
+        }
+        
+    }
+
+    public func dataForColumnIndex(_ columnIdx: Int32) -> Data? {
+        if (sqlite3_column_type(statement.statement, columnIdx) == SQLITE_NULL || (columnIdx < 0)) {
+            return nil;
+        }
+
+        let dataSize = sqlite3_column_bytes(statement.statement, columnIdx)
+        let data = Data(capacity: Int(dataSize))
+        data.copyBytes(to: unsafeBitCast(sqlite3_column_blob(statement.statement, columnIdx), to: UnsafeMutableRawBufferPointer.self), count: Int(dataSize))
+        return data
+    }
+
+    deinit {
+        statement.reset()
+
     }
 
     public static func resultSetWith(statement: FMStatement, aDB: FMDatabase) -> FMResultSet {
